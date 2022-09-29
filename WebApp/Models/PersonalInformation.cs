0705540e1fcd1519ef1af686b4cdc44625391ca2
Sergio Palomino <1195526+clstr@users.noreply.github.com>
2022-09-29 00:11:50 -0400

    Add project files.

---
+++ WebApp/Models/PersonalInformation.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApp.Models
+{
+    //public record PersonalInformation(string Name, string ShortDescription, List<Link> Links);
+    //public record Link(string LinkUrl, string Title, string Icon);
+    public class PersonalInformation
+    {
+        
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public List<Link> Links { get; set; }
+    
+    }
+    
+    public class Link
+    {
+        public Uri Url { get; set; }
+        public string Title { get; set; }
+        public string Icon { get; set; }
+    }
+}
